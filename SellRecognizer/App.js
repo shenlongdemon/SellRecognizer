07763d7145e8ef07b0909fac38cbb67454ea676a
CO7VF2D1G1HW <shenlongdemon@gmail.com>
2018-04-03 06:57:44 +0700

    using react-native-router-flux

--- SellRecognizer/App.js
+++ SellRecognizer/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Router, Scene } from 'react-native-router-flux';
 
+import ListItems from './screen/ListItems';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.1</Text>
-      </View>
+      <Router>
+      <Scene key="root">
+        <Scene key="listitems"
+          component={ListItems}
+          title="Your items"
+          initial          
+        />       
+      </Scene>
+    </Router>
     );
   }
 }
