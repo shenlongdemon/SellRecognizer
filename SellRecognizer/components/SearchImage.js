be786ca8c84761bc03013978ae496d1ac2562c02
Long Nguyen <>
2018-04-06 13:19:28 +0700

    Done for search image

--- SellRecognizer/components/SearchImage.js
+++ SellRecognizer/components/SearchImage.js
@@ -3,26 +3,9 @@ import WebApi from 'react-native-web-api';
 import config from '../config.json';
 import _ from "underscore";
 export default class SearchImage extends React.Component {
-  static getImages(keyword, complete) {
-
-    //"url":"?q={keyword}&num={pageSize}&start={offset}&imgSize=medium&searchType=image&key={APIKey}&cx={searchEngineKey}",
-    
-      var url = config.google.imageSearch.url;
-      url = url.replace("{keyword}", keyword);
-      url = url.replace("{pageSize}", 10);
-      url = url.replace("{offset}", 1);
-      url = url.replace("{APIKey}", config.google.APIKey);
-      url = url.replace("{searchEngineKey}", config.google.imageSearch.searchEngineKey);
-      console.log("BEGIN getImages " + url);
-      WebApi.request("GET",url,null,null).then(function(json){
-        console.log("DOING getImages" + JSON.stringify(json));
-
-        var items = _.map(json.items, function(item){ 
-            return {link:item.link, title: item.title};
-        });
-        console.log("END getImages " + items.length);
-        complete(json);
-      });
-
+  static getImages(keyword) {
+    console.log("SearchImage getImages " + keyword);
+    var url = config.service.url + "/api/sellrecognizer/searchImage?obj=" + keyword;
+    return WebApi.request("GET",url, null,null);
   }
 }
--- SellRecognizer/screen/SearchImageItem.js
+++ SellRecognizer/screen/SearchImageItem.js
@@ -1,29 +1,64 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
 import { Actions } from 'react-native-router-flux'; // New code
 import SearchImage from '../components/SearchImage'
+import GridView from 'react-native-super-grid';
 export default class SearchImageItem extends React.Component {
-  constructor(){
-    super();
-    alert();
-    SearchImage.getImages("rolex",function(items){
-      alert(item.length);
-    });
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Add Item</Text>
-      </View>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            keyword: "",
+            images: []
+        };
+    }
+
+    getImage(keyword) {
+        var self = this;
+        SearchImage.getImages("rolex")
+            .then(function (res) { // (A)      
+                console.log(JSON.stringify(res));
+                self.setState({ images: res.Data });
+            })
+            .catch(function (error) { // (B)        
+                alert("Error " + JSON.stringify(error));
+                console.error('An error occurred', error);
+            });
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <TextInput
+                    style={{ width: 100, height: 40, borderColor: 'gray', borderWidth: 1 }}
+                    onChangeText={(text) => this.setState({ keyword: text })}
+                    value={this.state.keywork}
+                />
+                <Text
+                    onPress={() => this.getImage(this.state.keywork)} // New Code
+                >Get Image</Text>
+
+                <GridView
+                    itemDimension={130}
+                    items={this.state.images}
+                    renderItem={item => (
+                        <View>
+
+                            <Image
+                                style={{ width: 50, height: 50 }}
+                                source={{ uri: item.link }}
+                            />
+                        </View>
+                    )}
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
